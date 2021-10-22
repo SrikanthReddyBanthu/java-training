e4d73873b4fb1ee74c4c13168e6446cef3693f3d
Srikanth Reddy Banthu <35705507+SrikanthReddyBanthu@users.noreply.github.com>
2021-10-22 12:19:27 +0530

    Bean Validations

--- demo/src/main/java/com/cjss/demo/controller/Controller1.java
+++ demo/src/main/java/com/cjss/demo/controller/Controller1.java
@@ -5,6 +5,7 @@ import com.cjss.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -15,7 +16,7 @@ public class Controller1 {
 
 
     @PostMapping("/add")
-    List<EmployeeModel> add(@RequestBody EmployeeModel newEmployee) {
+    List<EmployeeModel> add(@Valid @RequestBody EmployeeModel newEmployee) {
 
         return employeeService.add(newEmployee);
     }
--- demo/src/main/java/com/cjss/demo/model/EmployeeModel.java
+++ demo/src/main/java/com/cjss/demo/model/EmployeeModel.java
@@ -1,12 +1,22 @@
 package com.cjss.demo.model;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 public class EmployeeModel {
 
+    @Min(value = 1, message = "id should have min value of 1")
+    @Max(value = 10, message = "id should have max value as 10")
     private int id;
+    @NotNull(message = "Name should not be null")
     private String name;
+    @Pattern(regexp="^[a-zA-Z0-9]{3}",message="designation length must be 3")
     private String designation;
 
     public EmployeeModel(int id, String name, String designation) {
+
         this.id = id;
         this.name = name;
         this.designation = designation;
